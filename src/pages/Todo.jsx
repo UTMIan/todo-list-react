d22d920fcc9de515b0bb58fab405fd8aa4ddec7f
UTMIan <102817866+UTMIan@users.noreply.github.com>
2023-10-11 01:29:40 +0000

    Funciones ToDo

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,13 +1,32 @@
+import { useState, useRef } from "react";
 import ListItem from "../components/Listitem";
 
 
 function Todo(){
+    // useState -> [state, setState]
+    const [todos, setTodos] = useState([]);
+
+    const inputRef = useRef(null);
+    
+    // Add new ToDo
+    const addTodo = () => {
+        const todoValue = inputRef.current.value;
+        console.log(todoValue);
+        setTodos([...todos, todoValue]);
+    };
+
     return (
-    <div>
-        <ul>
-            <ListItem text="Item 1" />
-            <ListItem text="Item 2" />
-            <ListItem text="Item 3" />
+    <div className="flex flex-col gap-3">
+        <div className="">
+            <input ref={inputRef} type="text" className="rounded-md bg-[#424242] px-4 py-2 mt-3" />
+            <button onClick={addTodo} className="rounded-md bg-[#3fc516] ml-4 px-4 py-2">Add task</button>
+        </div>
+        <ul className="flex flex-col gap-3">
+            {
+                todos.map((value, index)=> {
+                    return <ListItem key={index} text={value} />
+                })
+            }
         </ul>
     </div>
     
