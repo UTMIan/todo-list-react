4ae64cd26ca9527c0c6ee8c618cf316b43e58391
UTMIan <102817866+UTMIan@users.noreply.github.com>
2023-10-12 16:58:14 +0000

    Crear, eliminar ToDo List

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
-function ListItem({text, checked, onDelete}){
+function ListItem({ text, checked, onDelete }) {
+  const [isChecked, setChecked] = useState(checked);
 
-    const[setChecked]=useState(false);
-    const handleCheck = () => {
-        setChecked(!checked)
-    }
+  const handleCheck = () => {
+    setChecked(!isChecked);
+  };
 
-    return (
-        <li className={`"p-2 bg-indigo-900" ${checked} ? 'line-throut'" : null`}>
-            {text}<input className="m-2" type="checkbox" checked={checked} onChange={handleCheck}></input>
-        </li>
-    )
-    
+  return (
+    <li className={`p-2 bg-[#2e2e2e86] rounded-md flex justify-between items-center ${isChecked ? 'line-through' : ''}`}>
+      <div>
+        <input className="m-2" type="checkbox" checked={isChecked} onChange={handleCheck} />
+        {text}
+      </div>
+      <div>
+        <button onClick={onDelete} className="rounded-md bg-[#b81d1d] px-2 py-1">
+          Delete
+        </button>
+      </div>
+    </li>
+  );
 }
 
 export default ListItem;
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,37 +1,36 @@
 import { useState, useRef } from "react";
 import ListItem from "../components/Listitem";
 
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
 
-function Todo(){
-    // useState -> [state, setState]
-    const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    const todoValue = inputRef.current.value;
+    setTodos([...todos, todoValue]);
+  };
 
-    const inputRef = useRef(null);
-    
-    // Add new ToDo
-    const addTodo = () => {
-        const todoValue = inputRef.current.value;
-        console.log(todoValue);
-        setTodos([...todos, todoValue]);
-    };
+  const deleteTodo = (index) => {
+    // Filtrar la lista de tareas y actualizar el estado
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
 
-    return (
+  return (
     <div className="flex flex-col gap-3">
-        <div className="">
-            <input ref={inputRef} type="text" className="rounded-md bg-[#424242] px-4 py-2 mt-3" />
-            <button onClick={addTodo} className="rounded-md bg-[#3fc516] ml-4 px-4 py-2">Add task</button>
-        </div>
-        <ul className="flex flex-col gap-3">
-            {
-                todos.map((value, index)=> {
-                    return <ListItem key={index} text={value} />
-                })
-            }
-        </ul>
+      <div className="">
+        <input ref={inputRef} type="text" className="rounded-md bg-[#424242] px-4 py-2 mt-3" />
+        <button onClick={addTodo} className="rounded-md bg-[#13cf33] ml-4 px-4 py-2">
+          Add task
+        </button>
+      </div>
+      <ul className="flex flex-col gap-3">
+        {todos.map((value, index) => (
+          <ListItem key={index} text={value} onDelete={() => deleteTodo(index)} />
+        ))}
+      </ul>
     </div>
-    
-    )
-    
-
+  );
 }
+
 export default Todo;
